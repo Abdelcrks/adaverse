2394ba947b6e2d776e5b1f9c3103eea45ea79c89
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-11-25 11:50:42 +0100

    feat: create tables with drizzle

---
+++ src/db/schema.ts
@@ -0,0 +1,29 @@
+import { pgTable, serial, text,date, integer} from "drizzle-orm/pg-core";
+
+export const categories = pgTable("categories", {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull().unique(),
+})
+
+export const promos = pgTable("promos", {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    dateStart: date("date_start").notNull(),
+})
+
+
+export const projects = pgTable("projects",{
+    id: serial("id").primaryKey(),
+    title: text("title").notNull(),
+    urlGitHub: text("url_github").notNull(),
+    urlDemo:text("url_demo"),
+    urlImage:text("url_image"),
+    categoryId:integer("category_id").references(()=> categories.id,{
+        onDelete: "set null",
+    }),
+    promoId:integer("promo_id").references(()=>promos.id,{
+        onDelete:"set null",
+    }),
+    createdAt: date("created_at").defaultNow().notNull(),
+    publishedAt:date("published_at")
+})
