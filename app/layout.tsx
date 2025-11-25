aba1da664e2105b8647707ce79042660fcb1954b
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-11-25 16:15:14 +0100

    add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import { Poppins, Montserrat } from "next/font/google";
 import "./globals.css";
+import { Header } from "./components/Header";
 
 export const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"], 
+  weight: ["400", "500", "600", "700"],
   variable: "--font-poppins",
 });
 
@@ -21,9 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} bg-[linear-gradient(356deg,rgba(0,0,0,1)_0%,rgba(112,61,61,1)_100%,rgba(0,0,0,1)_100%)]
-       text-white`}>
-        {children}
+      <body className={`${poppins.variable} bg-[linear-gradient(356deg,rgba(0,0,0,1)_0%,rgba(78,56,56,1)_100%,rgba(0,0,0,1)_100%)] text-white max-w-6xl mx-auto`}>
+        <Header/>
+        <div className="min-h-screen flex justify-center">
+          {children}
+        </div>
       </body>
     </html>
   );
