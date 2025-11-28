bba1c21be7b96389526fee98291bf95673dfda73
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-11-28 10:40:50 +0100

    add responsive for mobile

--- app/components/ProjectForm.tsx
+++ app/components/ProjectForm.tsx
@@ -5,26 +5,26 @@ import { createProject } from "@/src/db/action";
 
 
 
-export const ProjectForm = ({onClose}: {onClose :() => void}) => {
+export const ProjectForm = ({ onClose }: { onClose: () => void }) => {
     // const [error, setError] = useState("")
-    const [formProject, setFormProject] = useState({ 
+    const [formProject, setFormProject] = useState({
         title: "",
         github: "",
-        demo : "",
-        promo : "",
-        theme : "",
+        demo: "",
+        promo: "",
+        theme: "",
     });
 
 
-    const isFormValid = 
-    formProject.title.trim() !== "" && 
-    formProject.github.trim() !== "" && 
-    formProject.promo.trim() !== "" && 
-    formProject.theme.trim() !== ""  
-    
+    const isFormValid =
+        formProject.title.trim() !== "" &&
+        formProject.github.trim() !== "" &&
+        formProject.promo.trim() !== "" &&
+        formProject.theme.trim() !== ""
+
 
     const handleAction = async (formData: FormData) => {
-        if(!isFormValid)
+        if (!isFormValid)
             return
         await createProject(formData)
         onClose
@@ -61,51 +61,87 @@ export const ProjectForm = ({onClose}: {onClose :() => void}) => {
     // }
 
     return (
-        <div className="flex justify-center">
-            <form action={handleAction}
-            className="relative mt-10 p-10 w-[450px] bg-white text-black flex flex-col items-center gap-10 rounded-2xl"
-            >
-                <button className="absolute left-4 top-4 text-red-600 text-xl cursor-pointer"
-                onClick={onClose}
-                >❌</button>
-                <h1 className="text-2xl">Publier un projet</h1>
+        <div className="flex justify-center px-4">
+            <form
+                action={handleAction}
+                className="
+              relative 
+              mt-10 
+              w-full max-w-md 
+              p-6 md:p-10 
+              bg-white text-black 
+              flex flex-col items-center gap-6 
+              rounded-2xl shadow-xl
+            ">
+                <button className="absolute left-4 top-4 text-red-600 text-xl cursor-pointer" onClick={onClose}>❌</button>
+                <h1 className="text-2xl font-semibold">Publier un projet</h1>
 
-                <label htmlFor="title" >Titre</label>
-                <input type="text"  id="title" name="title" placeholder="Quizz Marvel"  required
-                value={formProject.title}
-                onChange={(e) => setFormProject((prev)=>({...prev, title: e.target.value}))}/>
-                <label htmlFor="github">Lien Github</label>
-                <input type="url" id="github"name="github" placeholder="www.github.com" required
-                value={formProject.github}
-                onChange={(e) => setFormProject((prev)=>({...prev, github: e.target.value}))}/>
-                <label htmlFor="demo">Lien Démo</label>
-                <input type="url" id="demo" name="demo" placeholder="www.quizz-marvel.vercel.app"
-                value={formProject.demo}
-                onChange={(e) => setFormProject((prev)=>({...prev, demo: e.target.value}))} />
+                <div className="w-full flex flex-col gap-1">
+                    <label htmlFor="title">Titre</label>
+                    <input type="text" id="title" name="title" placeholder="Quizz Marvel" required
+                        className="w-full border border-gray-300 rounded-lg px-4 py-2"
+                        value={formProject.title}
+                        onChange={(e) =>
+                            setFormProject((prev) => ({ ...prev, title: e.target.value }))
+                        }/>
+                </div>
 
-                <label htmlFor="promo">Promo</label>
-                <select name="promo" id="promo" required
-                value={formProject.promo}
-                onChange={(e) => setFormProject((prev)=>({...prev, promo: e.target.value}))}>
-                    <option value="1">Frida</option>
-                    <option value="2">Graces</option>
-                </select>
+                <div className="w-full flex flex-col gap-1">
+                    <label htmlFor="github">Lien Github</label>
+                    <input type="url" id="github" name="github" placeholder="www.github.com" required
+                        className="w-full border border-gray-300 rounded-lg px-4 py-2"
+                        value={formProject.github}
+                        onChange={(e) =>
+                            setFormProject((prev) => ({ ...prev, github: e.target.value }))
+                        }/>
+                </div>
 
-                <label htmlFor="theme">Catégorie</label>
-                <select name="theme" id="theme" required
-                value={formProject.theme}
-                onChange={(e) => setFormProject((prev)=>({...prev, theme: e.target.value}))}>
-                    <option value="1">Quizz</option>
-                    <option value="2">Dataviz</option>
-                    <option value="3">Adacheckevent</option>
-                </select>
+                <div className="w-full flex flex-col gap-1">
+                    <label htmlFor="demo">Lien Démo</label>
+                    <input type="url" id="demo" name="demo" placeholder="www.quizz-marvel.vercel.app"
+                    className="w-full border border-gray-300 rounded-lg px-4 py-2"
+                        value={formProject.demo}
+                        onChange={(e) =>
+                            setFormProject((prev) => ({ ...prev, demo: e.target.value }))
+                        }/>
+                </div>
 
-                <button  type="submit" disabled={!isFormValid}
-                className={`p-3.5 text-white rounded-full ${
-                    isFormValid ? "bg-orange-500 cursor-pointer" : "bg-gray-400 cursor-not-allowed"
-                  }`}>
-                    Envoyer</button>
+                <div className="w-full flex flex-col gap-1">
+                    <label htmlFor="promo">Promo</label>
+                    <select name="promo" id="promo" required
+                        className="w-full border border-gray-300 rounded-lg px-4 py-2"
+                        value={formProject.promo}
+                        onChange={(e) =>
+                            setFormProject((prev) => ({ ...prev, promo: e.target.value }))
+                        }>
+                        <option value="">Sélectionner</option>
+                        <option value="1">Frida</option>
+                        <option value="2">Graces</option>
+                    </select>
+                </div>
+
+                <div className="w-full flex flex-col gap-1">
+                    <label htmlFor="theme">Catégorie</label>
+                    <select name="theme" id="theme" required
+                        className="w-full border border-gray-300 rounded-lg px-4 py-2"
+                        value={formProject.theme}
+                        onChange={(e) =>
+                            setFormProject((prev) => ({ ...prev, theme: e.target.value }))
+                        }>
+                        <option value="">Sélectionner</option>
+                        <option value="1">Quizz</option>
+                        <option value="2">Dataviz</option>
+                        <option value="3">Adacheckevent</option>
+                    </select>
+                </div>
+
+                <button
+                    type="submit" disabled={!isFormValid}
+                    className={`w-full py-3 rounded-full text-white transition 
+                        ${isFormValid ? "bg-orange-500 hover:bg-orange-600": "bg-gray-400 cursor-not-allowed"}`}>
+                    Envoyer
+                </button>
             </form>
         </div>
-    )
-}
+    );
+};
