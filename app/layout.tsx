66dc8488ee601413865178f43fb5fb513f9d2fb4
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-11-30 18:29:53 +0100

    change css global

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} bg-[linear-gradient(356deg,rgba(0,0,0,1)_0%,rgba(78,56,56,1)_100%,rgba(0,0,0,1)_100%)] text-white max-w-6xl mx-auto`}>
+      <body className={`${poppins.variable} bg-[linear-gradient(356deg,rgba(0,0,0,1)_0%,rgba(78,56,56,1)_100%,rgba(0,0,0,1)_100%)] text-white`}>
         <Header/>
-        <main className="min-h-screen w-full flex justify-center">
+        <main className="">
         <div className="w-full max-w-6xl mx-auto">
           {children}
         </div>
