86de433bff16e181e5b1c47e28f4761e68ee2940
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-11-30 19:04:53 +0100

    add routes by next, select for select categories slug

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,13 +2,28 @@
 import Link from "next/link"
 import { useState } from "react";
 import { ProjectForm } from "./ProjectForm";
+import { useRouter } from "next/navigation";
+
 
 // type HeaderProps = {
 //     onClose: () => void
 // }
 
-export const Header = () => {
+interface Category {
+    id: number,
+    name: string,
+}
+
+export const Header = ({categories} : {categories : Category[]}) => {
+    const router = useRouter()
     const [showForm, setShowForm] = useState(false);
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = e.target.value;
+        if (!value) return;
+        router.push(`/categories/${value}`); // value = name (quizz, dataviz…)
+      };
 
     return (
         <div>
@@ -16,9 +31,20 @@ export const Header = () => {
                 <Link href={"/"} className="text-4xl font-semibold text-white">ada<span className="text-orange-300">VERSE</span></Link>
 
                 <div className="flex items-center gap-8">
-                    <Link href={"/"} className="rounded-full cursor-pointer  px-3 py-2.5 text-sm ">
-                        TOUT LES PROJETS ⌄
-                    </Link>
+                <select
+          defaultValue=""
+          onChange={handleChange}
+          className="bg-transparent border border-white/40 rounded-full px-4 py-1 text-sm outline-none"
+        >
+          <option value="" disabled>
+            Choisir une catégorie
+          </option>
+          {categories.map((cat) => (
+            <option key={cat.id} value={cat.name}>
+              {cat.name}
+            </option>
+          ))}
+        </select>
                     <button
                         className=" bg-orange-500 px-5 py-2.5  cursor-pointer text-sm font-medium text-white"
                         onClick={() => { setShowForm((show) => !show), console.log("etat", showForm) }}>
