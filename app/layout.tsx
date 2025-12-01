3fb540929224f384b84bec6a0867d449a422edf3
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-12-01 15:16:09 +0100

    add componants and props themeprovider in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
-import type { Metadata } from "next";
 import { Poppins, Montserrat } from "next/font/google";
 import "./globals.css";
 import { Header } from "./components/Header";
 import { categories } from "@/src/db/schema";
-import { AllCategories } from "./components/AllCategories";
 import { db } from "@/src/db";
+import { ThemeProvider } from "./components/theme-provider";
 
 
 export const poppins = Poppins({
@@ -30,12 +29,14 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.variable} bg-[linear-gradient(356deg,rgba(0,0,0,1)_0%,rgba(78,56,56,1)_100%,rgba(0,0,0,1)_100%)] text-white max-w-6xl mx-auto`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
         <Header categories={allCategories}/>
         <main className="min-h-screen w-full">
         <div className="w-full max-w-6xl mx-auto">
           {children}
         </div>
         </main>
+        </ThemeProvider>
       </body>
     </html>
   );
