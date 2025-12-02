38838dae97602569fd9aeaddd9e188c05ec81bc6
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-12-02 10:33:12 +0100

    change baseurl

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -19,7 +19,7 @@ export const ProjectCard = async ({project}:{project:projectCardProps}) => {
      const image = project.urlImage ?? "/images/default-thumbnail.png"
 
 
-     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000";
+     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
      const githubInfo = await fetch(`${baseUrl}/api/github-info?url=${project.urlGitHub}`,
        { next: { revalidate: 3600 } }
